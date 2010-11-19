8d3c287f4b0857dd3d8f63560e9d37c45cc829d0
Andreas Zwinkau <zwinkau@kit.edu>
2010-11-19 13:21:29 +0100

    Refactoring: introduces GoalSequence

--- x10.firm/src/x10firm/ExtensionInfo.java
+++ x10.firm/src/x10firm/ExtensionInfo.java
@@ -10,6 +10,7 @@ import polyglot.util.ErrorQueue;
 import polyglot.visit.PostCompiled;
 import x10firm.goals.AsmEmitted;
 import x10firm.goals.FirmGenerated;
+import x10firm.goals.GoalSequence;
 import x10firm.goals.OptimizedFirm;
 import x10firm.types.TypeSystem;
 
@@ -78,31 +79,29 @@ public class ExtensionInfo extends x10.ExtensionInfo {
 
 		@Override
 		public Goal CodeGenerated(Job job) {
-			TypeSystem typeSystem = (TypeSystem)extInfo.typeSystem();
-			Goal firm_generated = new FirmGenerated(job, typeSystem);
-			firm_generated.intern(this);
+			final GoalSequence seq = new GoalSequence("FirmSequence", job);
 			
-			Goal optimized_firm = new OptimizedFirm(job, typeSystem);
+			TypeSystem typeSystem = (TypeSystem)extInfo.typeSystem();
+			
+			final Goal firm_generated = new FirmGenerated(job, typeSystem);
+			firm_generated.intern(this);
+			seq.append(firm_generated);
+			
+			final Goal optimized_firm = new OptimizedFirm(job, typeSystem);
 			optimized_firm.addPrereq(firm_generated);
 			optimized_firm.intern(this);
+			seq.append(optimized_firm);
 			
-			AsmEmitted asm_emitted = new AsmEmitted(job);
+			final AsmEmitted asm_emitted = new AsmEmitted(job);
 			/* Technically we could output unoptimized Firm as well.
 			 * However, it is the responsibility of the OptimizedFirm goal
 			 * to decide whether and which optimizations to apply.
 			 * Therefore, always require OptimizedFirm */
 			asm_emitted.addPrereq(optimized_firm);
 			asm_emitted.intern(this);
+			seq.append(asm_emitted);
 			
-			/* We must return asm_emitted, so the X10Scheduler will
-			 * call "end_goal.addPrereq(asm_emitted)".
-			 * Nevertheless, all "asm_emitted.addPrereq(X)" calls,
-			 * must be redirected to "firm_generated.addPrereq(X)",
-			 * otherwise firm_generated would have no dependencies,
-			 * which lets it crash.
-			 */
-			asm_emitted.redirectPrereq(firm_generated);
-			return asm_emitted;
+			return seq;
 		}
 	}
 }
---
+++ x10.firm/src/x10firm/goals/GoalSequence.java
@@ -0,0 +1,61 @@
+package x10firm.goals;
+
+import polyglot.frontend.Goal;
+import polyglot.frontend.Job;
+import polyglot.frontend.SourceGoal_c;
+
+/**
+ * This is a meta goal, which really consists of a linear sequence of goals.
+ * 
+ * We only need this class to work around the Polyglot scheduler architecture,
+ * where the CodeGenerated method can only return one goal.
+ */
+public class GoalSequence extends SourceGoal_c {
+
+	/** constructor calls super -- make Java happy */
+	public GoalSequence(String name, Job job) {
+		super(name, job);
+	}
+
+	/**
+	 * First goal in the sequence.
+	 * All prerequirements must be redirected to this goal.
+	 */
+	private Goal first = null;
+	
+	/**
+	 * Last goal in the sequence.
+	 * However, 'this' is really the last goal in the sequence. 
+	 */
+	private Goal last = null;
+
+	@Override
+	public boolean runTask() {
+		/* nothing to do itself, all functionality is in the sequence it depends on */
+		return true;
+	}
+	
+	@Override
+	public void addPrereq(Goal goal) {
+		/* redirect to the first goal, since all other goals transitively get the prereq as well then. */
+		first.addPrereq(goal);
+	}
+	
+	/** append another goal to the end of the goal sequence */
+	public void append(Goal goal) {
+		/* depend on all goals in the sequence */
+		super.addPrereq(goal);
+		
+		if (first == null) {
+			first = goal;
+			return;
+		}
+		
+		if (last == null) {
+			goal.addPrereq(first);
+		} else {
+			goal.addPrereq(last);
+		}
+		last = goal;
+	}
+}
