50072bf3dc3e0d1476ad31b3ceeba218888691e6
Andreas Zwinkau <zwinkau@kit.edu>
2010-11-23 15:13:29 +0100

    Refactor: extract goal class Linked

--- x10.firm/src/x10firm/X10FirmScheduler.java
+++ x10.firm/src/x10firm/X10FirmScheduler.java
@@ -1,19 +1,12 @@
 package x10firm;
 
-import java.util.ArrayList;
-
-import polyglot.frontend.Compiler;
 import polyglot.frontend.Goal;
 import polyglot.frontend.Job;
-import polyglot.main.Options;
-import polyglot.util.ErrorInfo;
-import polyglot.util.ErrorQueue;
-import polyglot.visit.PostCompiled;
 import x10.ExtensionInfo.X10Scheduler;
-import x10cpp.visit.X10CPPTranslator;
 import x10firm.goals.AsmEmitted;
 import x10firm.goals.FirmGenerated;
 import x10firm.goals.GoalSequence;
+import x10firm.goals.Linked;
 import x10firm.goals.OptimizedFirm;
 import x10firm.goals.SourceGoalSequence;
 import x10firm.types.TypeSystem;
@@ -35,40 +28,19 @@ class X10FirmScheduler extends X10Scheduler {
 	protected Goal PostCompiled() {
 		/*
 		 * The other X10 backends and Polyglot use this goal to invoke javac/gcc
-		 * the generated source code. In the Firm context this corresponds to li
-		 * the assembler output.
+		 * the generated source code. In the Firm context this corresponds to
+		 * emitting assembler and linking with the stdlib.
 		 */
-		final Goal postCompiled = new PostCompiled(extInfo) {
-			@Override
-			protected boolean invokePostCompiler(Options options,
-					Compiler compiler, ErrorQueue eq) {
-				CompilerOptions opts = (CompilerOptions) options;
-				String exe_name = opts.exe_name;
-				String asm_name = AsmEmitted.ASM_FILENAME;
-				// TODO link our standard library correctly
-				String[] cmd = {"gcc",asm_name,"stdlib/primitive_types.c","stdlib/simple_lib.c","-lm","-o",exe_name};
-
-				/* C++ backend decides according to options, whether to delete the output files */
-				ArrayList<String> output_files = new ArrayList<String>();
-				output_files.add(asm_name);
-
-				/* reuse the C++ backend */
-				if (! X10CPPTranslator.doPostCompile(options, eq, output_files, cmd)) {
-					eq.enqueue(ErrorInfo.POST_COMPILER_ERROR, "linking failed");
-					return false;
-				}
-				return true;
-			}
-		};
-		postCompiled.intern(this);
 
 		final Goal asmEmitted = new AsmEmitted(this);
-		postCompiled.addPrereq(asmEmitted);
 		asmEmitted.intern(this);
+
+		final Goal linked = new Linked(extInfo);
+		linked.intern(this);
 
 		final GoalSequence seq = new GoalSequence("FirmOutputSequence");
 		seq.append(asmEmitted);
-		seq.append(postCompiled);
+		seq.append(linked);
 
 		return seq.intern(this);
 	}
---
+++ x10.firm/src/x10firm/goals/Linked.java
@@ -0,0 +1,43 @@
+package x10firm.goals;
+
+import java.util.ArrayList;
+
+import polyglot.frontend.Compiler;
+import polyglot.frontend.ExtensionInfo;
+import polyglot.main.Options;
+import polyglot.util.ErrorInfo;
+import polyglot.util.ErrorQueue;
+import polyglot.visit.PostCompiled;
+import x10cpp.visit.X10CPPTranslator;
+import x10firm.CompilerOptions;
+
+/**
+ * Final Goal, which links the generated asm with the stdlib
+ */
+public class Linked extends PostCompiled {
+	/** constructor */
+	public Linked(ExtensionInfo extInfo) {
+		super(extInfo);
+	}
+
+	@Override
+	protected boolean invokePostCompiler(Options options,
+			Compiler compiler, ErrorQueue eq) {
+		CompilerOptions opts = (CompilerOptions) options;
+		String exe_name = opts.exe_name;
+		String asm_name = AsmEmitted.ASM_FILENAME;
+		// TODO link our standard library correctly
+		String[] cmd = {"gcc",asm_name,"stdlib/primitive_types.c","stdlib/simple_lib.c","-lm","-o",exe_name};
+
+		/* C++ backend decides according to options, whether to delete the output files */
+		ArrayList<String> output_files = new ArrayList<String>();
+		output_files.add(asm_name);
+
+		/* reuse the C++ backend */
+		if (! X10CPPTranslator.doPostCompile(options, eq, output_files, cmd)) {
+			eq.enqueue(ErrorInfo.POST_COMPILER_ERROR, "linking failed");
+			return false;
+		}
+		return true;
+	}
+}
