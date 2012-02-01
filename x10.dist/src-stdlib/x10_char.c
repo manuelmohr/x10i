50706fece84b4b2b2e3ed80faeab8a8da81dfa56
Andreas Zwinkau <zwinkau@kit.edu>
2012-02-01 16:04:34 +0100

    fix type errors in stdlib

--- x10.dist/src-stdlib/x10_char.c
+++ x10.dist/src-stdlib/x10_char.c
@@ -3,9 +3,9 @@
 #include "x10_string.h"
 
 X10_MAKE_COMPARETO(_ZN3x104lang4Char9compareToEDi, x10_char)
-X10_MAKE_HASHCODE(_ZN3x104lang4Char8hashCodeEv, x10_char)
+X10_MAKE_HASHCODE(_ZN3x104lang4Char8hashCodeEDi, x10_char)
 X10_MAKE_EQUALS(_ZN3x104lang4Char6equalsEDi, x10_char)
-X10_MAKE_TOSTRING(_ZN3x104lang4Char8toStringEv, x10_char, "%lc")
+X10_MAKE_TOSTRING(_ZN3x104lang4Char8toStringEDi, x10_char, "%lc")
 
 MAKE_INT_BINOPS(_ZN3x104lang4Char, Di, x10_char)
 MAKE_CMPOPS(_ZN3x104lang4Char, Di, x10_char)
