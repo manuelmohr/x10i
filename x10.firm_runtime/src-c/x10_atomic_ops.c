c61bc61f04aed39b2d1ef5f40a41bf62fe823995
Matthias Braun <matze@braunis.de>
2012-02-13 14:10:06 +0100

    avoid internal linux pthread symbols

--- x10.firm_runtime/src-c/x10_atomic_ops.c
+++ x10.firm_runtime/src-c/x10_atomic_ops.c
@@ -1,21 +1,35 @@
 #include "x10_atomic_ops.h"
 
-static pthread_mutex_t _longOperationLock = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
+static pthread_mutexattr_t attr;
+static pthread_mutex_t     opLock;
 
 void x10_atomic_ops_lock()
 {
-	pthread_mutex_lock(&_longOperationLock);
+	pthread_mutex_lock(&opLock);
 }
 
 void x10_atomic_ops_unlock()
 {
-    pthread_mutex_unlock(&_longOperationLock);
+    pthread_mutex_unlock(&opLock);
+}
+
+static void __attribute__((constructor)) init_locks(void)
+{
+	pthread_mutexattr_init(&attr);
+	pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+	pthread_mutex_init(&opLock, &attr);
+}
+
+static void __attribute__((destructor)) destroy_locks(void)
+{
+	pthread_mutex_destroy(&opLock);
+	pthread_mutexattr_destroy(&attr);
 }
 
 /**
-* Ensure that all loads before the barrier have loaded their
-* data before any load after the data accesses its data.
-*/
+ * Ensure that all loads before the barrier have loaded their
+ * data before any load after the data accesses its data.
+ */
 void _ZN3x104util10concurrent6Fences15loadLoadBarrierEv() {
 #if (defined(_ARCH_PPC) || defined(_ARCH_450) || defined(_ARCH_450d))
    x10_atomic_ops_ppc_sync(); /* TODO: sync is overkill for this barrier */
@@ -23,10 +37,10 @@ void _ZN3x104util10concurrent6Fences15loadLoadBarrierEv() {
 }
 
 /**
-* Ensure that all loads before the barrier have loaded
-* their data before any data stored by a store after
-* the barrier has been flushed.
-*/
+ * Ensure that all loads before the barrier have loaded
+ * their data before any data stored by a store after
+ * the barrier has been flushed.
+ */
 void _ZN3x104util10concurrent6Fences16loadStoreBarrierEv() {
 #if (defined(_ARCH_PPC) || defined(_ARCH_450) || defined(_ARCH_450d))
    x10_atomic_ops_ppc_isync();
@@ -34,10 +48,10 @@ void _ZN3x104util10concurrent6Fences16loadStoreBarrierEv() {
 }
 
 /**
-* Ensure that all data from stores before the barrier
-* has been flushed before any data for loads after the
-* barrier is accessed.
-*/
+ * Ensure that all data from stores before the barrier
+ * has been flushed before any data for loads after the
+ * barrier is accessed.
+ */
 void _ZN3x104util10concurrent6Fences16storeLoadBarrierEv() {
 #if (defined(_ARCH_PPC) || defined(_ARCH_450) || defined(_ARCH_450d))
    x10_atomic_ops_ppc_sync();
@@ -45,13 +59,12 @@ void _ZN3x104util10concurrent6Fences16storeLoadBarrierEv() {
 }
 
 /**
-* Ensure that all data from stores before the barrier
-* has been flushed before any data for stores after
-* the barrier is flushed.
-*/
+ * Ensure that all data from stores before the barrier
+ * has been flushed before any data for stores after
+ * the barrier is flushed.
+ */
 void _ZN3x104util10concurrent6Fences17storeStoreBarrierEv() {
 #if (defined(_ARCH_PPC) || defined(_ARCH_450) || defined(_ARCH_450d))
    x10_atomic_ops_ppc_lwsync();
 #endif
 }
-
