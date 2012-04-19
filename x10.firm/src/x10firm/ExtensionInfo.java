9b1b115ba3c7ea176f3a773fdbb230976f7cb833
Matthias Braun <matthias.braun@kit.edu>
2012-04-19 15:28:27 +0200

    sort whitelist entries

--- x10.firm/src/x10firm/ExtensionInfo.java
+++ x10.firm/src/x10firm/ExtensionInfo.java
@@ -55,51 +55,44 @@ public class ExtensionInfo extends x10.ExtensionInfo {
 	// TODO: DELETE ME: Need library support
 	private static Set<String> allowedFiles = new HashSet<String>();
 	static {
+		allowedFiles.add("x10/compiler/CompilerFlags.x10");
+		allowedFiles.add("x10/compiler/InitDispatcher.x10");
 		allowedFiles.add("x10/lang/Any.x10");
-		allowedFiles.add("x10/lang/Object.x10");
-
-		allowedFiles.add("x10/lang/UInt.x10");
-		allowedFiles.add("x10/lang/UByte.x10");
-		allowedFiles.add("x10/lang/UShort.x10");
-		allowedFiles.add("x10/lang/ULong.x10");
-		allowedFiles.add("x10/lang/Short.x10");
-		allowedFiles.add("x10/lang/Byte.x10");
-		allowedFiles.add("x10/lang/Boolean.x10");
-		allowedFiles.add("x10/lang/Long.x10");
-		allowedFiles.add("x10/lang/Int.x10");
-		allowedFiles.add("x10/lang/Float.x10");
-		allowedFiles.add("x10/lang/Double.x10");
-		allowedFiles.add("x10/lang/Char.x10");
-		allowedFiles.add("x10/lang/Pointer.x10");
-		allowedFiles.add("x10/lang/String.x10");
-
-		allowedFiles.add("x10/lang/LongRange.x10");
-
-		allowedFiles.add("x10/lang/Zero.x10");
-
-		allowedFiles.add("x10/lang/Indexable.x10");
-		allowedFiles.add("x10/lang/Comparable.x10");
-		allowedFiles.add("x10/lang/Bitwise.x10");
 		allowedFiles.add("x10/lang/Arithmetic.x10");
+		allowedFiles.add("x10/lang/Bitwise.x10");
+		allowedFiles.add("x10/lang/Boolean.x10");
+		allowedFiles.add("x10/lang/Byte.x10");
+		allowedFiles.add("x10/lang/Char.x10");
+		allowedFiles.add("x10/lang/Comparable.x10");
+		allowedFiles.add("x10/lang/Double.x10");
+		allowedFiles.add("x10/lang/Float.x10");
+		allowedFiles.add("x10/lang/Indexable.x10");
+		allowedFiles.add("x10/lang/Int.x10");
 		allowedFiles.add("x10/lang/Iterable.x10");
-		allowedFiles.add("x10/lang/NativeSupport.x10");
-
 		allowedFiles.add("x10/lang/Iterable.x10");
 		allowedFiles.add("x10/lang/Iterator.x10");
+		allowedFiles.add("x10/lang/LongRange.x10");
+		allowedFiles.add("x10/lang/Long.x10");
+		allowedFiles.add("x10/lang/NativeSupport.x10");
+		allowedFiles.add("x10/lang/Object.x10");
+		allowedFiles.add("x10/lang/Pointer.x10");
+		allowedFiles.add("x10/lang/Short.x10");
+		allowedFiles.add("x10/lang/String.x10");
 		allowedFiles.add("x10/lang/Thread.x10");
-
-		allowedFiles.add("x10/util/concurrent/AtomicFloat.x10");
+		allowedFiles.add("x10/lang/UByte.x10");
+		allowedFiles.add("x10/lang/UInt.x10");
+		allowedFiles.add("x10/lang/ULong.x10");
+		allowedFiles.add("x10/lang/UShort.x10");
+		allowedFiles.add("x10/lang/Zero.x10");
+		allowedFiles.add("x10/util/concurrent/AtomicBoolean.x10");
 		allowedFiles.add("x10/util/concurrent/AtomicDouble.x10");
+		allowedFiles.add("x10/util/concurrent/AtomicFloat.x10");
 		allowedFiles.add("x10/util/concurrent/AtomicInteger.x10");
 		allowedFiles.add("x10/util/concurrent/AtomicLong.x10");
-		allowedFiles.add("x10/util/concurrent/AtomicBoolean.x10");
 		allowedFiles.add("x10/util/concurrent/Fences.x10");
 		allowedFiles.add("x10/util/concurrent/Lock.x10");
 		allowedFiles.add("x10/util/Container.x10");
 		allowedFiles.add("x10/util/IndexedMemoryChunk.x10");
-
-		allowedFiles.add("x10/compiler/CompilerFlags.x10");
-		allowedFiles.add("x10/compiler/InitDispatcher.x10");
 	}
 
 	/** returns true if it is expected, that the given class compiles cleanly
