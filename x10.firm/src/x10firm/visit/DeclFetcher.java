838656e0e40384efc424afe70f519b21ba469c9d
Matthias Braun <matthias.braun@kit.edu>
2012-04-27 17:34:21 +0200

    rework DeclFetcher, fix some missing Job problems

--- x10.firm/src/x10firm/visit/DeclFetcher.java
+++ x10.firm/src/x10firm/visit/DeclFetcher.java
@@ -3,20 +3,21 @@ package x10firm.visit;
 import polyglot.ast.Node;
 import polyglot.ast.TypeNode;
 import polyglot.frontend.Job;
+import polyglot.types.ClassDef;
+import polyglot.types.ClassType;
+import polyglot.types.ConstructorDef;
 import polyglot.types.ContainerType;
 import polyglot.types.MemberDef;
-import polyglot.types.Ref;
+import polyglot.types.MethodDef;
 import polyglot.types.Type;
 import polyglot.types.Types;
 import polyglot.util.Position;
 import polyglot.visit.NodeVisitor;
-import x10.ast.X10Call;
 import x10.ast.X10ClassDecl;
+import x10.ast.X10ConstructorDecl;
 import x10.ast.X10MethodDecl;
 import x10.ast.X10SourceFile_c;
 import x10.types.X10ClassDef;
-import x10.types.X10ClassType;
-import x10.types.X10MethodDef;
 
 /**
  * Finds the declaration, i.e. the ClassDecl node, for a given class.
@@ -37,21 +38,28 @@ public final class DeclFetcher {
 	 * @param candidate the constructor definition whose container is desired
 	 * @return the definition of the X10 Class containing the constructor
 	 */
-	private static X10ClassDef getContainer(final MemberDef candidate) {
-		final Ref<? extends ContainerType> containerRef = candidate.container();
-		final ContainerType containerType = Types.get(containerRef);
+	private static ClassDef getContainer(final MemberDef candidate) {
+		final ContainerType containerType = candidate.container().get();
 		final Type containerBase = Types.baseType(containerType);
-		assert containerBase instanceof X10ClassType;
-		final X10ClassDef container = ((X10ClassType) containerBase).x10Def();
+		assert containerBase instanceof ClassType;
+		final X10ClassDef container = ((ClassType) containerBase).def();
 		return container;
 	}
 
-	private static Job getJob(final X10ClassDef container) {
+	private static Job getJob(final ClassDef container) {
 		final Job job = container.job();
-		if (job == null)
+		if (job == null) {
+			if (container.isNested()) {
+				return getJob(container.outer().get());
+			}
+
+			System.err.println(container.toString() + " has no job associated");
 			return null;
-		if (!job.extensionInfo().scheduler().shouldCompile(job))
+		}
+		if (!job.extensionInfo().scheduler().shouldCompile(job)) {
+			System.err.println("Won't fetch decl for " + container + " (not marked for compilation)");
 			return null;
+		}
 
 		final Node ast = job.ast();
 		if (ast == null) {
@@ -93,7 +101,7 @@ public final class DeclFetcher {
 					return n; // we've already found the decl, short-circuit search
 				if (n instanceof TypeNode)
 					return n; // TypeNodes don't contain decls, short-circuit search
-				if (!pos.isCompilerGenerated() && !contains(n.position(), pos))
+				if (pos != null && !pos.isCompilerGenerated() && !contains(n.position(), pos))
 					return n; // definition of md isn't inside n, short-circuit search
 				if (n instanceof X10ClassDecl
 						&& candidate == ((X10ClassDecl) n).classDef()) {
@@ -127,7 +135,7 @@ public final class DeclFetcher {
 	 * @return the declaration for the indicated method, or null if no
 	 *         declaration can be found or it has an empty body.
 	 */
-	private static X10MethodDecl getMethodDeclaration(final X10MethodDef candidate, final Node ast) {
+	private static X10MethodDecl getMethodDeclaration(final MethodDef candidate, final Node ast) {
 		final Position pos = candidate.position();
 		final X10MethodDecl[] decl = new X10MethodDecl[1];
 
@@ -170,21 +178,72 @@ public final class DeclFetcher {
 		return decl[0];
 	}
 
-	private static Node getAST(final X10ClassDef container) {
+	/**
+	 * Walk an AST looking for the declaration of a given method.
+	 *
+	 * @param candidate
+	 *            the method whose declaration is desired
+	 * @param ast
+	 *            the abstract syntax tree containing the declaration
+	 * @return the declaration for the indicated method, or null if no
+	 *         declaration can be found or it has an empty body.
+	 */
+	private static X10ConstructorDecl getConstructorDeclaration(final ConstructorDef candidate, final Node ast) {
+		final Position pos = candidate.position();
+		final X10ConstructorDecl[] decl = new X10ConstructorDecl[1];
+
+		ast.visit(new NodeVisitor() {
+			private boolean contains(final Position outer, final Position inner) {
+				if (!outer.file().equals(inner.file()))
+					return false;
+				if (!outer.path().equals(inner.path()))
+					return false;
+				return (outer.offset() <= inner.offset() && inner.endOffset() <= inner.endOffset());
+			}
+
+			@Override
+			public polyglot.ast.Node override(final polyglot.ast.Node n) {
+				if (null != decl[0])
+					return n; // we've already found the decl, short-circuit search
+				if (n instanceof TypeNode)
+					return n; // TypeNodes don't contain decls, short-circuit search
+				if (!pos.isCompilerGenerated() && !contains(n.position(), pos))
+					return n; // definition of md isn't inside n, short-circuit search
+				if (n instanceof X10ConstructorDecl
+						&& candidate == ((X10ConstructorDecl) n).constructorDef()) {
+					decl[0] = (X10ConstructorDecl) n;
+					return n; // we found the decl for the candidate, short-circuit search
+				}
+				return null; // look for the decl inside n
+			}
+		});
+
+		if (null == decl[0]) {
+			System.err.println("declaration not found for " + candidate);
+			return null;
+		}
+
+		if (!decl[0].flags().flags().isNative() && null == decl[0].body()) {
+			System.err.println("no declaration body for " + decl[0] + " (" + candidate + ")");
+			return null;
+		}
+
+		return decl[0];
+	}
+
+	private static Node getAST(final ClassDef container) {
 		if (null == container) {
-			System.err.println("unable to find container for candidate: " + container);
 			return null;
 		}
 
 		final Job candidateJob = getJob(container);
 		if (null == candidateJob) {
-			System.err.println("unable to find job for candidate: " + container);
 			return null;
 		}
 
 		final Node ast = candidateJob.ast();
 		if (null == ast) {
-			System.err.println("unable to find ast for candidated: " + container);
+			System.err.println("unable to find ast for candidate: " + container);
 			return null;
 		}
 		return ast;
@@ -197,15 +256,14 @@ public final class DeclFetcher {
 	 * @return The declaration of the method invoked by call, or null if the
 	 *         declaration cannot be found.
 	 */
-	public static X10ClassDecl getDecl(final X10ClassType n) {
-		final X10ClassDef candidate = n.def();
-		final Node ast = getAST(candidate);
+	public static X10ClassDecl getDecl(final X10ClassDef def) {
+		final Node ast = getAST(def);
 		if (ast == null)
 			return null;
 
-		final X10ClassDecl decl = getClassDeclaration(candidate, ast);
+		final X10ClassDecl decl = getClassDeclaration(def, ast);
 		if (null == decl) {
-			System.err.println("unable to find declaration for candidate: " + n);
+			System.err.println("unable to find declaration for candidate: " + def);
 			return null;
 		}
 
@@ -213,25 +271,34 @@ public final class DeclFetcher {
 	}
 
 	/**
-	 * Get the declaration corresponding to a call.
-	 *
-	 * @param call
-	 *            The call to the method we need the declaration for.
-	 * @return The declaration of the method invoked by call, or null if the
-	 *         declaration cannot be found.
+	 * Get the declaration corresponding to a ConstructorDef.
 	 */
-	public static X10MethodDecl getDecl(final X10Call call) {
+	public static X10ConstructorDecl getDecl(final ConstructorDef def) {
+		final ClassDef container = getContainer(def);
+		final Node ast = getAST(container);
+		if (ast == null)
+			return null;
+		final X10ConstructorDecl decl = getConstructorDeclaration(def, ast);
+		if (decl == null) {
+			System.err.println("unable to find declaration for constructor: " + def);
+			return null;
+		}
+		return decl;
+	}
+
+	/**
+	 * Get the declaration corresponding to a MethodDef.
+	 */
+	public static X10MethodDecl getDecl(final MethodDef methodDef) {
 		// get candidate
-		final X10MethodDef candidate = call.methodInstance().x10Def();
-		final X10ClassDef container = getContainer(candidate);
+		final ClassDef container = getContainer(methodDef);
 		final Node ast = getAST(container);
 		if (ast == null)
 			return null;
 
-		final X10MethodDecl decl = getMethodDeclaration(candidate, ast);
+		final X10MethodDecl decl = getMethodDeclaration(methodDef, ast);
 		if (null == decl) {
-			System.err.println("unable to find declaration for candidate: "
-					+ candidate);
+			System.err.println("unable to find declaration for candidate: " + methodDef);
 			return null;
 		}
 
