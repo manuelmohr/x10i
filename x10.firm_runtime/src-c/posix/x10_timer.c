5a014c29e21dc3bc70bba484c23948647454e70b
Matthias Braun <matthias.braun@kit.edu>
2013-01-11 14:20:46 +0100

    fix indentation

--- x10.firm_runtime/src-c/posix/x10_timer.c
+++ x10.firm_runtime/src-c/posix/x10_timer.c
@@ -4,14 +4,14 @@
 
 x10_long _ZN3x104util5Timer8nanoTimeEv()
 {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (x10_long)(tv.tv_sec * 1000000000LL + tv.tv_usec * 1000LL);
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (x10_long)(tv.tv_sec * 1000000000LL + tv.tv_usec * 1000LL);
 }
 
 x10_long _ZN3x104util5Timer9milliTimeEv()
 {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (x10_long)(tv.tv_sec * 1000LL + tv.tv_usec / 1000);
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (x10_long)(tv.tv_sec * 1000LL + tv.tv_usec / 1000);
 }
