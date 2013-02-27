9ee629080e17435061fb150d4283024395091927
Matthias Braun <matthias.braun@kit.edu>
2013-02-27 14:19:25 +0100

    fix alloc_object bug from XMALLOC changes

--- x10.firm_runtime/src-c/x10_object.c
+++ x10.firm_runtime/src-c/x10_object.c
@@ -8,7 +8,7 @@ x10_object *x10_alloc_object(const x10_vtable *vptr, size_t size)
 {
 	assert(size > 0);
 	assert(vptr->runtime_type_info->size == size);
-	x10_object *o = XMALLOC(x10_object);
+	x10_object *o = (x10_object*)xmalloc(size);
 	o->vptr = vptr;
 	return o;
 }
