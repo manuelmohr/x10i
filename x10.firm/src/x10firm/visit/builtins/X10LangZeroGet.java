4d704cc641b02cb61e3aa73fda70a69a3bef1fca
Matthias Braun <matthias.braun@kit.edu>
2013-03-06 20:17:43 +0100

    simplify size_T type guessing by using mode_P
    (This should also be a bit more robust, since size_t is rather as big as
     a pointer than as the default machine word for arithmetic)

--- x10.firm/src/x10firm/visit/builtins/X10LangZeroGet.java
+++ x10.firm/src/x10firm/visit/builtins/X10LangZeroGet.java
@@ -3,22 +3,20 @@ package x10firm.visit.builtins;
 import java.util.Collections;
 import java.util.List;
 
-import com.sun.jna.Pointer;
-
 import polyglot.types.LocalInstance;
 import polyglot.types.Type;
-import polyglot.util.Position;
 import x10.types.MethodInstance;
 import x10firm.types.FirmTypeSystem;
-import x10firm.visit.CodeGenError;
 import x10firm.visit.FirmGenerator;
 import x10firm.visit.MethodConstruction;
+
+import com.sun.jna.Pointer;
+
 import firm.CompoundType;
 import firm.Entity;
 import firm.Ident;
 import firm.MethodType;
 import firm.Mode;
-import firm.bindings.binding_be;
 import firm.bindings.binding_iroptimize;
 import firm.nodes.Call;
 import firm.nodes.Node;
@@ -27,29 +25,10 @@ import firm.nodes.Node;
  * Create instance of built-in {@code static x10.lang.Zero.get[T](): T} method.
  */
 class X10LangZeroGet implements BuiltinMethodGenerator {
-	private static Mode getSizeTMode() {
-		final int machineSize = binding_be.be_get_machine_size() / 8;
-
-		switch (machineSize) {
-		case 1:
-			return Mode.getBu();
-		case 2:
-			return Mode.getHu();
-		case 4:
-			return Mode.getIu();
-		case 8:
-			return Mode.getLu();
-		case 16:
-			return Mode.getLLu();
-		default:
-			throw new CodeGenError("Unknown machine size: " + machineSize, Position.COMPILER_GENERATED);
-		}
-	}
-
 	private static MethodType getMemsetMethodType() {
 		final firm.Type pointer = Mode.getP().getType();
 		final firm.Type signedInt = Mode.getIs().getType();
-		final firm.Type sizeT = getSizeTMode().getType();
+		final firm.Type sizeT = Mode.getP().getType();
 		final firm.Type[] parameterTypes = new firm.Type[]{pointer, signedInt, sizeT};
 		final firm.Type[] resultTypes = new firm.Type[]{pointer};
 
@@ -89,7 +68,7 @@ class X10LangZeroGet implements BuiltinMethodGenerator {
 					binding_iroptimize.create_compilerlib_entity(memsetIdent.ptr, memsetType.ptr);
 			final Entity memsetEntity = new Entity(memsetEntPtr);
 			final Node memsetAddress = con.newSymConst(memsetEntity);
-			final Node structSize = con.newSymConstTypeSize(firmType, getSizeTMode());
+			final Node structSize = con.newSymConstTypeSize(firmType, Mode.getP());
 			final Node zero = con.newConst(0, Mode.getIs());
 			final Node[] memsetArgs = new Node[]{tmpStruct, zero, structSize};
 			final Node call = con.newCall(con.getCurrentMem(), memsetAddress, memsetArgs, memsetType);
