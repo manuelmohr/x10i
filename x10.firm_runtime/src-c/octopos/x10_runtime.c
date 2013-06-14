bb90e7a71b4fb541bcc392aae9d66c237763b39a
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2013-06-14 11:03:52 +0200

    Removed superfluous includes.

--- x10.firm_runtime/src-c/octopos/x10_runtime.c
+++ x10.firm_runtime/src-c/octopos/x10_runtime.c
@@ -1,5 +1,4 @@
 #include "x10_runtime.h"
-#include "init.h"
 
 // x10.lang.Runtime.getHere() : Int
 x10_int _ZN3x104lang7Runtime9getHereIdEv()
--- x10.firm_runtime/src-c/places.h
+++ x10.firm_runtime/src-c/places.h
@@ -2,8 +2,6 @@
 #ifndef PLACES_H
 #define PLACES_H
 
-#include "types.h"
-
 /** the highest possible tile ID */
 extern unsigned n_places;
 
