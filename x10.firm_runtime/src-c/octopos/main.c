fa071e94d49f59a5c7f878dd2ddcae2799dd5c82
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2013-06-14 10:47:07 +0200

    Invade all tiles.

--- x10.firm_runtime/src-c/octopos/main.c
+++ x10.firm_runtime/src-c/octopos/main.c
@@ -3,26 +3,47 @@
 #include "main.h"
 #include "init.h"
 #include "places.h"
+#include "places_octopos.h"
 
 #if __GNUC__ > 4 || (__GNUC__==4 && __GNUC_MINOR__>=7)
 #error Error: gcc-4.7 and above are not supported by octopos (yet?)
 #endif
 
-/* single tile octopos only has place 0 */
-unsigned n_places = 1;
+unsigned       n_places;
+proxy_claim_t *places;
 
 void main_ilet(claim_t claim)
 {
 	/* We want to use uart redirection through grmon -u */
 	leon_set_uart_debug_mode(1);
 
+	n_places = get_tile_count();
+	places   = mem_allocate(MEM_TLM_GLOBAL, n_places * sizeof(*places));
+
+	unsigned place_id = 0;
+
 	/*
-	 * Get as many cpus as possible.
+	 * Get as many CPUs as possible.
 	 * Remove this once invading is exposed as an API call to the user.
 	 */
-	uint32_t num = 64;
-	while (num > 0 && invade_simple(claim, num) != 0)
-		--num;
+	for (unsigned tile_id = 0; tile_id < n_places; tile_id++) {
+		uint32_t        num = 4;
+		invade_future_t fut;
+
+		/* Skip I/O tile. */
+		if (tile_id == get_io_tile_id())
+			continue;
+
+		proxy_claim_t claim;
+		while (num > 0 && (proxy_invade(tile_id, &fut, num) != 0 || (claim = invade_future_force(&fut)) == 0))
+			--num;
+
+		assert(num > 0 && "Could not invade tile");
+
+		places[place_id++] = claim;
+	}
+
+	n_places = place_id;
 
 	x10_static_initializer();
 
---
+++ x10.firm_runtime/src-c/octopos/places_octopos.h
@@ -0,0 +1,9 @@
+/* platform specific place abstraction */
+#ifndef PLACES_OCTOPOS_H
+#define PLACES_OCTOPOS_H
+
+#include <octopos.h>
+
+extern proxy_claim_t *places;
+
+#endif
