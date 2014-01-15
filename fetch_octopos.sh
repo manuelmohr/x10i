43bb6470811751e093ee621659712cef7ac2c83c
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2014-01-15 16:40:05 +0100

    Updated OctoPOS version for more parallelism.
    This fixes agent/InvadeTest.x10.

--- fetch_octopos.sh
+++ fetch_octopos.sh
@@ -6,9 +6,9 @@ set -eu
 
 RELEASES=""
 RELEASES="$RELEASES release.leon.4t-sf-chipit-w-iotile.2013-12-02.tar.bz2"
-RELEASES="$RELEASES release.x86guest.multitile.2013-12-24.tar.bz2"
+RELEASES="$RELEASES release.x86guest.multitile.2014-01-15.tar.bz2"
 MD5SUMS="""8e4240dbe931e6af08824d3f61f1442c  release.leon.4t-sf-chipit-w-iotile.2013-12-02.tar.bz2
-302cb56afc4342eabf2f8c8cb355100f  release.x86guest.multitile.2013-12-24.tar.bz2"""
+186477a37e4d1c7e902d3a8acf421e5b  release.x86guest.multitile.2014-01-15.tar.bz2"""
 
 DIST_DIR="octopos-dist"
 OCTOPOS_APP_DIR="octopos-app"
