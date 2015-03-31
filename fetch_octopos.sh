49c90518edb48f93a98db3bfcdda9756a877abcf
Manuel Mohr <manuel.mohr@kit.edu>
2015-03-31 13:35:21 +0200

    Upgrade OctoPOS version.
    New version does not print agent debug information.

--- fetch_octopos.sh
+++ fetch_octopos.sh
@@ -7,14 +7,14 @@
 #    https://www4.cs.fau.de/invasic/octopos/
 set -euv
 
-RELEASE_DATE="2015-03-04"
+RELEASE_DATE="2015-03-31"
 DEFAULT_X86GUEST="4t-w-iotile"
 DEFAULT_LEON="4t5c-chipit-w-iotile"
 RELEASES=""
 RELEASES="$RELEASES release.leon.${DEFAULT_LEON}.${RELEASE_DATE}.tar.bz2"
 RELEASES="$RELEASES release.x86guest.${DEFAULT_X86GUEST}.${RELEASE_DATE}.tar.bz2"
-MD5SUMS="""0848e8ed5f74896a5c85fd46db824556  release.leon.4t5c-chipit-w-iotile.2015-03-04.tar.bz2
-54adf7b384918269964db31757340831  release.x86guest.4t-w-iotile.2015-03-04.tar.bz2"""
+MD5SUMS="""933f24d042d01090c1c07dfdd68ddc01  release.leon.4t5c-chipit-w-iotile.2015-03-31.tar.bz2
+f145bee41239a354b2a5f4288529e220  release.x86guest.4t-w-iotile.2015-03-31.tar.bz2"""
 
 DIST_DIR="octopos-dist"
 OCTOPOS_APP_DIR="octopos-app"
