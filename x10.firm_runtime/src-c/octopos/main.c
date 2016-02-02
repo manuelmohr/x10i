a825c2361f6512bfea97745c2db9a312b37042c6
Manuel Mohr <manuel.mohr@kit.edu>
2016-02-02 12:31:46 +0100

    Remove unused local variables to silence warning.

--- x10.firm_runtime/src-c/octopos/main.c
+++ x10.firm_runtime/src-c/octopos/main.c
@@ -77,7 +77,6 @@ static void ilet_dma_local_finish(void *context)
 
 static void ilet_dma_remote_finish(void *pid)
 {
-	claim_t cl = get_claim();
 	place_local_data *pld = claim_get_local_data();
 	pld->place_id = (unsigned)(uintptr_t)pid;
 }
@@ -112,7 +111,6 @@ static void ilet_transfer_places(void *remote_places, void *context)
 static void ilet_allocate_places(void *arg_n_places, void *context)
 {
 	unsigned n_places = (unsigned)(uintptr_t)arg_n_places;
-	claim_t cl = get_claim();
 	place_local_data *pld = claim_get_local_data();
 	if (NULL == pld) {
 		pld = mem_allocate_tlm(sizeof(place_local_data));
@@ -159,7 +157,6 @@ void distribute_places(dispatch_claim_t *new_places, unsigned new_n_places)
 /* initalize X10 an all places */
 static void init_all_places(void)
 {
-	claim_t cl = get_claim();
 	place_local_data *pld = claim_get_local_data();
 	unsigned n_places = pld->n_places;
 	simple_signal_init(&initialization_signal, n_places);
@@ -232,7 +229,6 @@ static void init_places(claim_t root_claim)
 		proxy_retreat(pclaim, &fut);
 		retreat_future_force(&fut);
 	}
-	claim_t cl = get_claim();
 	place_local_data *pld = claim_get_local_data();
 	pld->n_places = 1;
 #endif
