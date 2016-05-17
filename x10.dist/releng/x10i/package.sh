f0c8f47ef21a2a36a51bb2c04893ec78cf3be6ef
Christoph Erhardt <erhardt@cs.fau.de>
2016-05-17 10:18:02 +0200

    Allow passing parameters through the package.sh script.

--- x10.dist/releng/x10i/package.sh
+++ x10.dist/releng/x10i/package.sh
@@ -10,14 +10,14 @@ cd "$top"
 
 # Build x10i for all available targets.
 # NOTE: We assume that the working copy is clean.
-./setup.sh -DVARIANT=optimize -DTARGET=i686-invasic-octopos
-./setup.sh -DVARIANT=optimize -DTARGET=i686-invasic-irtss
-./setup.sh -DVARIANT=optimize -DTARGET=i686-linux-gnu
-./setup.sh -DVARIANT=optimize -DTARGET=sparc-invasic-octopos
-./setup.sh -DVARIANT=optimize -DTARGET=sparc-invasic-irtss
-./setup.sh -DVARIANT=optimize -DTARGET=x86_64-invasic-octopos
-./setup.sh -DVARIANT=optimize -DTARGET=x86_64-invasic-irtss
-./setup.sh -DVARIANT=optimize -DTARGET=x86_64-linux-gnu
+./setup.sh $@ -DVARIANT=optimize -DTARGET=i686-invasic-octopos
+./setup.sh $@ -DVARIANT=optimize -DTARGET=i686-invasic-irtss
+./setup.sh $@ -DVARIANT=optimize -DTARGET=i686-linux-gnu
+./setup.sh $@ -DVARIANT=optimize -DTARGET=sparc-invasic-octopos
+./setup.sh $@ -DVARIANT=optimize -DTARGET=sparc-invasic-irtss
+./setup.sh $@ -DVARIANT=optimize -DTARGET=x86_64-invasic-octopos
+./setup.sh $@ -DVARIANT=optimize -DTARGET=x86_64-invasic-irtss
+./setup.sh $@ -DVARIANT=optimize -DTARGET=x86_64-linux-gnu
 
 # Pack everything together.
 tar czf "$cdir/$mydir/x10i.tar.gz" --files-from="$cdir/$mydir/distfiles.txt" \
