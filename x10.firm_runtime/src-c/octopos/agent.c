556711930b2701406e4f3372c78311be588493f4
Andreas Zwinkau <zwinkau@kit.edu>
2016-09-02 10:52:47 +0200

    Fixup Quickfix
    Quickfix assumed a reinvade from within a claim, thus failed for a
    some normal invades or reinvade from outside a claim.

--- x10.firm_runtime/src-c/octopos/agent.c
+++ x10.firm_runtime/src-c/octopos/agent.c
@@ -26,43 +26,61 @@ void agentclaim_set_current(agentclaim_t ac)
 }
 
 #ifdef USE_AGENTSYSTEM
-static void redistribute_places(agentclaim_t ac)
+static void dispatch_claim_list_from_agentclaim(agentclaim_t ac, dispatch_claim_t** old_places, int* old_n_places)
 {
-	assert (ac != NULL && "invalid claim"); // TODO throw exception?
+	assert(*old_places == NULL);
 	const int new_n_places = agent_claim_get_tilecount(ac);
-	if (0 == new_n_places)
-		return; /* nothing to distribute */
-	dispatch_claim_t *new_places = mem_allocate_tlm(new_n_places * sizeof(*new_places));
+	assert (new_n_places > 0);
+	dispatch_claim_t *const new_places = mem_allocate_tlm(new_n_places * sizeof(*new_places));
 	assert(NULL != new_places);
 	/* collect proxyclaims and convert to dispatch claims */
-	for (int it = 0; it < new_n_places; ++it) {
-		int tid = agent_claim_get_tileid_iterative(ac, it);
+	for (int i = 0; i < new_n_places; i++) {
+		int tid = agent_claim_get_tileid_iterative(ac, i);
 		proxy_claim_t pClaim = agent_claim_get_proxyclaim_tile_type(ac, tid, 0);
 		assert (pClaim != NULL);
 		dispatch_claim_t dc = proxy_get_dispatch_info(pClaim);
-		new_places[it] = dc;
+		new_places[i] = dc;
 	}
-	/* reorder, so new places are always added to the back */
-	place_local_data *pld = claim_get_local_data();
-	dispatch_claim_t* old_places = pld->places;
-	const int old_n_places = pld->n_places;
-	assert (old_n_places <= new_n_places && "only ever add places (sticky!)");
-	for (int i = 0; i < old_n_places; i++) {
-		const tile_id_t oi = dispatch_claim_get_tid(old_places[i]);
-		const tile_id_t ni = dispatch_claim_get_tid(new_places[i]);
-		if (oi == ni)
-			continue;
-		int j = i+1;
-		for (; j < new_n_places; j++) {
-			const tile_id_t nj = dispatch_claim_get_tid(new_places[j]);
-			if (oi != nj)
+	*old_places = new_places;
+	*old_n_places = new_n_places;
+}
+
+
+static void redistribute_places(agentclaim_t ac, dispatch_claim_t* old_places, int old_n_places)
+{
+	assert (ac != NULL && "invalid claim"); // TODO throw exception?
+	dispatch_claim_t *new_places = NULL;
+	int new_n_places = 0;
+	dispatch_claim_list_from_agentclaim(ac, &new_places, &new_n_places);
+	assert(NULL != new_places);
+	assert(new_n_places > 0);
+	if (0 == new_n_places)
+		return; /* nothing to distribute */
+	if (old_places != NULL) {
+		/* reinvade: reorder, so new places are always added to the back */
+		assert (old_n_places <= new_n_places && "only ever add places (sticky!)");
+		//printf("reorder %d new according to %d old\n", new_n_places, old_n_places);
+		for (int i = 0; i < old_n_places; i++) {
+			const tile_id_t oi = dispatch_claim_get_tid(old_places[i]);
+			const tile_id_t ni = dispatch_claim_get_tid(new_places[i]);
+			//printf("cmp at %d: %d ?= %d\n", i, oi, ni);
+			if (oi == ni)
 				continue;
-			dispatch_claim_t tmp = new_places[i];
-			new_places[i] = new_places[j];
-			new_places[j] = tmp;
-			break;
+			int j = i+1;
+			//printf("search from %d to %d\n", j, new_n_places);
+			for (; j < new_n_places; j++) {
+				const tile_id_t nj = dispatch_claim_get_tid(new_places[j]);
+				//printf("nj == %d\n", nj);
+				if (oi != nj)
+					continue;
+				//printf("found %d at %d\n", nj, j);
+				dispatch_claim_t tmp = new_places[i];
+				new_places[i] = new_places[j];
+				new_places[j] = tmp;
+				break;
+			}
+			assert (j < new_n_places && "found something to swap" );
 		}
-		assert (j < new_n_places && "found something to swap" );
 	}
 	/* distribute array to all tiles */
 	distribute_places(new_places, new_n_places);
@@ -74,7 +92,7 @@ agentclaim_t x10_agent_claim_invade(agent_t parentagent, constraints_t constr)
 {
 	agentclaim_t ac = agent_claim_invade(parentagent, constr);
 	if (NULL != ac)
-		redistribute_places(ac);
+		redistribute_places(ac, NULL, 0);
 	return ac;
 }
 
@@ -84,7 +102,7 @@ agentclaim_t x10_agent_claim_invade_or_constraints(agent_t parentagent, uint8_t 
 {
 	agentclaim_t ac = agent_claim_invade_or_constraints(parentagent, constr_count, constr);
 	if (NULL != ac)
-		redistribute_places(ac);
+		redistribute_places(ac, NULL, 0);
 	return ac;
 }
 
@@ -94,7 +112,7 @@ agentclaim_t x10_agent_claim_invade_parentclaim(agentclaim_t parentclaim, constr
 {
 	agentclaim_t ac = agent_claim_invade_parentclaim(parentclaim, constr);
 	if (NULL != ac)
-		redistribute_places(ac);
+		redistribute_places(ac, NULL, 0);
 	return ac;
 }
 
@@ -102,9 +120,12 @@ agentclaim_t x10_agent_claim_invade_parentclaim(agentclaim_t parentclaim, constr
  * which does X10 specific stuff */
 int x10_agent_claim_reinvade(agentclaim_t claim)
 {
+	dispatch_claim_t *old_places = NULL;
+	int old_n_places = 0;
+	dispatch_claim_list_from_agentclaim(claim, &old_places, &old_n_places);
 	int r = agent_claim_reinvade(claim);
 	if (r > 0)
-		redistribute_places(claim);
+		redistribute_places(claim, old_places, old_n_places);
 	return r;
 }
 
@@ -112,9 +133,12 @@ int x10_agent_claim_reinvade(agentclaim_t claim)
  * which does X10 specific stuff */
 int x10_agent_claim_reinvade_constraints(agentclaim_t claim, constraints_t constr)
 {
+	dispatch_claim_t *old_places = NULL;
+	int old_n_places = 0;
+	dispatch_claim_list_from_agentclaim(claim, &old_places, &old_n_places);
 	int r = agent_claim_reinvade_constraints(claim, constr);
 	if (r > 0)
-		redistribute_places(claim);
+		redistribute_places(claim, old_places, old_n_places);
 	return r;
 }
 #endif
