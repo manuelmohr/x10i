00ab042c7ddcfea9bfb1de0fdaa57593f28e9133
Andreas Zwinkau <zwinkau@kit.edu>
2016-12-19 11:29:22 +0100

    Update OctoPOS to 2016-12-16

--- octopos_config.sh
+++ octopos_config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-RELEASE_DATE="2016-09-15"
+RELEASE_DATE="2016-12-16"
 DEFAULT_X86GUEST="4t-w-iotile"
 DEFAULT_LEON="4t5c-chipit-w-iotile"
 RELEASES=""
 RELEASES="$RELEASES release.leon.${DEFAULT_LEON}.${RELEASE_DATE}.tar.bz2"
 RELEASES="$RELEASES release.x86guest.${DEFAULT_X86GUEST}.${RELEASE_DATE}.tar.bz2"
 RELEASES="$RELEASES release.x64native.generic.${RELEASE_DATE}.tar.bz2"
-MD5SUMS="""ebcc4c40415c58056c0ae551e8d71bd1  release.leon.${DEFAULT_LEON}.${RELEASE_DATE}.tar.bz2
-1e9cff0b774126cf76cd19d6c473c2ac  release.x64native.generic.${RELEASE_DATE}.tar.bz2
-4b20ce6a2882f5f97132a6595768bbd4  release.x86guest.${DEFAULT_X86GUEST}.${RELEASE_DATE}.tar.bz2"""
+MD5SUMS="""f3d215187ee64a238bb1f32bd14fc9c3  release.leon.${DEFAULT_LEON}.${RELEASE_DATE}.tar.bz2
+7cea75e05b87cd520ee936956b72e205  release.x64native.generic.${RELEASE_DATE}.tar.bz2
+c6cf39ca9cff6982706361459541cd66  release.x86guest.${DEFAULT_X86GUEST}.${RELEASE_DATE}.tar.bz2"""
 
 DIST_DIR="octopos-dist"
 OCTOPOS_APP_DIR="octopos-app"
