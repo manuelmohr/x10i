59d4b07357ba429c998f525808121b757261c38e
Andreas Zwinkau <zwinkau@kit.edu>
2017-03-23 11:15:30 +0100

    Update iRTSS to 2017-03-22

--- octopos_config.sh
+++ octopos_config.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-RELEASE_DATE="2016-12-16"
+RELEASE_DATE="2017-03-22"
 DEFAULT_X86GUEST="4t-w-iotile"
 DEFAULT_LEON="4t5c-chipit-w-iotile"
 RELEASES=""
 RELEASES="$RELEASES release.leon.${DEFAULT_LEON}.${RELEASE_DATE}.tar.bz2"
 RELEASES="$RELEASES release.x86guest.${DEFAULT_X86GUEST}.${RELEASE_DATE}.tar.bz2"
 RELEASES="$RELEASES release.x64native.generic.${RELEASE_DATE}.tar.bz2"
-MD5SUMS="""f3d215187ee64a238bb1f32bd14fc9c3  release.leon.${DEFAULT_LEON}.${RELEASE_DATE}.tar.bz2
-7cea75e05b87cd520ee936956b72e205  release.x64native.generic.${RELEASE_DATE}.tar.bz2
-c6cf39ca9cff6982706361459541cd66  release.x86guest.${DEFAULT_X86GUEST}.${RELEASE_DATE}.tar.bz2"""
+MD5SUMS="""
+53f24235eef36202b097d6dde280c3c0  release.leon.4t5c-chipit-w-iotile.2017-03-22.tar.bz2
+371f4d3c594ddee0d99e3c12d0b08f4e  release.x64native.generic.2017-03-22.tar.bz2
+c33c422e6dd4a009b44ebe7d0cc90a14  release.x86guest.4t-w-iotile.2017-03-22.tar.bz2
+"""
 
 DIST_DIR="octopos-dist"
 OCTOPOS_APP_DIR="octopos-app"
