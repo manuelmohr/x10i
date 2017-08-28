3fff07a16caecd82ca81bec5b559c92dd3bf3c27
Andreas Fried <andreas.fried@kit.edu>
2017-08-28 13:23:34 +0200

    Allocate the recursive_spinlock of an x10.util.concurrent.Lock in TLM.

--- x10.firm_runtime/src-c/octopos/x10_lock.c
+++ x10.firm_runtime/src-c/octopos/x10_lock.c
@@ -3,25 +3,26 @@
 /* x10.util.concurrent.Lock.this(): Lock */
 void _ZN3x104util10concurrent4LockC1Ev(x10_lock *self)
 {
-	recursive_spinlock_init(&self->lock);
+	self->lock = mem_allocate_tlm(sizeof(recursive_spinlock));
+	recursive_spinlock_init(self->lock);
 }
 
 /* Lock.lock() */
 void _ZN3x104util10concurrent4Lock4lockEv(x10_lock *self)
 {
-	recursive_spinlock_lock(&self->lock);
+	recursive_spinlock_lock(self->lock);
 }
 
 /* Lock.tryLock(): Boolean */
 x10_boolean _ZN3x104util10concurrent4Lock7tryLockEv(x10_lock *self)
 {
-	return recursive_spinlock_trylock(&self->lock) == 1;
+	return recursive_spinlock_trylock(self->lock) == 1;
 }
 
 /* Lock.unlock() */
 void _ZN3x104util10concurrent4Lock6unlockEv(x10_lock *self)
 {
-	recursive_spinlock_unlock(&self->lock);
+	recursive_spinlock_unlock(self->lock);
 }
 
 x10_int _ZN3x104util10concurrent4Lock12getHoldCountEv(x10_lock *self)
